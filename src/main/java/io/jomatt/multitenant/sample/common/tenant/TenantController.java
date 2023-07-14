2f710f2ca28a0eb8daa6687eae1eecf964e3716d
Mohamad Atwa <mohamad.atwa@student.hu.nl>
2023-07-14 12:41:44 +0200

    adding client

--- src/main/java/io/jomatt/multitenant/sample/common/tenant/TenantController.java
+++ src/main/java/io/jomatt/multitenant/sample/common/tenant/TenantController.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tenants")
@@ -31,7 +32,8 @@ public class TenantController {
     }
 
     @PostMapping
-    public ResponseEntity<AdminTokenDto> createTenant(@RequestBody TenantDto tenantDto) {
+    public Tenant createTenant(@RequestBody TenantDto tenantDto) throws InterruptedException {
+        System.out.println("555555555");
             Tenant t = new Tenant(tenantDto);
             tenantRepository.save(t);
             EntityManager entityManager = entityManagerFactory.createEntityManager();
@@ -39,28 +41,111 @@ public class TenantController {
             entityManager.createNativeQuery("CREATE SCHEMA " + tenantDto.getName()).executeUpdate();
             entityManager.getTransaction().commit();
             entityManager.close();
+        System.out.println("88888888888888888");
 
+        String adminToken = getAdminToken();
+        String realmName = createKeyLoakTenant(adminToken, t.getName());
+        Thread.sleep(5000); // Sleep for 5 seconds (5000 milliseconds)
+        createClient(realmName, adminToken);
+
+        return t;
+    }
+
+    private String getAdminToken(){
+        String tokenUrl = "http://localhost:9090/realms/master/protocol/openid-connect/token";
+        String clientId = "admin-cli";
+        String username = "fofo";
+        String password = "fofo";
+        String grantType = "password";
+
+        // First, send the request to get the access token
+        HttpHeaders tokenHeaders = new HttpHeaders();
+        tokenHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> tokenBody = new LinkedMultiValueMap<>();
+        tokenBody.add("client_id", clientId);
+        tokenBody.add("username", username);
+        tokenBody.add("password", password);
+        tokenBody.add("grant_type", grantType);
+
+        HttpEntity<MultiValueMap<String, String>> tokenRequestEntity = new HttpEntity<>(tokenBody, tokenHeaders);
 
         RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object> tokenResponseEntity = restTemplate.exchange(
+                tokenUrl,
+                HttpMethod.POST,
+                tokenRequestEntity,
+                Object.class
+        );
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//        headers.add("PRIVATE-TOKEN", "xyz");
-
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("client_id","admin-cli");
-        map.add("grant_type","password");
-        map.add("username","fofo");
-        map.add("password","fofo");
-
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
-
-        ResponseEntity<AdminTokenDto> response =
-                restTemplate.exchange("http://localhost:9090/realms/master/protocol/openid-connect/token",
-                        HttpMethod.POST,
-                        entity,
-                        AdminTokenDto.class);
-        System.out.println(response.toString());
-        return response;
+        // Extract the access token from the response
+        Map<String, Object> responseBody = (Map<String, Object>) tokenResponseEntity.getBody();
+        return (String) responseBody.get("access_token");
     }
+    private String createKeyLoakTenant(String adminToken, String realmId){
+        String createRealmUrl = "http://localhost:9090/admin/realms";
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Create the request body for the second request
+        HttpHeaders createRealmHeaders = new HttpHeaders();
+        createRealmHeaders.setContentType(MediaType.APPLICATION_JSON);
+        createRealmHeaders.setBearerAuth(adminToken);
+
+//        String createRealmRequestBody = "{ \"id\": \"newrealm\", \"realm\": \"newrealm\", \"displayName\": \"New Realm\", \"enabled\": true, \"sslRequired\": \"external\", \"registrationAllowed\": false, \"loginWithEmailAllowed\": true, \"duplicateEmailsAllowed\": false, \"resetPasswordAllowed\": false, \"editUsernameAllowed\": false, \"bruteForceProtected\": true }";
+        String createRealmRequestBody = String.format("{ \"id\": \"%s\", \"realm\": \"%s\", \"displayName\": \"New Realm\", \"enabled\": true, \"sslRequired\": \"external\", \"registrationAllowed\": false, \"loginWithEmailAllowed\": true, \"duplicateEmailsAllowed\": false, \"resetPasswordAllowed\": false, \"editUsernameAllowed\": false, \"bruteForceProtected\": true }", realmId, realmId);
+
+
+        HttpEntity<String> createRealmRequestEntity = new HttpEntity<>(createRealmRequestBody, createRealmHeaders);
+
+        // Send the second request to create the realm
+        ResponseEntity<String> createRealmResponseEntity = restTemplate.exchange(
+                createRealmUrl,
+                HttpMethod.POST,
+                createRealmRequestEntity,
+                String.class
+        );
+
+        // Handle the response of the second request as needed
+        HttpStatus createRealmStatusCode = createRealmResponseEntity.getStatusCode();
+        String createRealmResponseBody = createRealmResponseEntity.getBody();
+
+        System.out.println("Create Realm Status Code: " + createRealmStatusCode);
+        System.out.println("Create Realm Response Body: " + createRealmResponseBody);
+        return realmId;
+
+    }
+
+
+    private void createClient(String realmName, String adminToken){
+        String apiUrl = "http://localhost:9090/admin/realms/" + realmName + "/clients";
+        String clientId = "app";
+        System.out.println(apiUrl);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(adminToken);
+
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("clientId", clientId);
+        requestBody.add("id", "app");
+
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+
+        // Handle the response as needed
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        String responseBody = responseEntity.getBody();
+
+        System.out.println("Response Status Code: " + statusCode);
+        System.out.println("Response Body: " + responseBody);
+    }
+    private void createUser(){}
+    private void createRole(){}
+    private void createGroup(){}
 }
--- src/test/resources/create-tenants.sql
+++ src/test/resources/create-tenants.sql
@@ -19,3 +19,8 @@ VALUES ('tenant1', 'Tenant 1', 'tenant1', 'https://idp.example.org/tenant-1'),
 
 CREATE SCHEMA IF NOT EXISTS "tenant1";
 CREATE SCHEMA IF NOT EXISTS "tenant2";
+
+UPDATE tenant
+SET issuer = 'http://localhost:9090/realms/tenant-1' WHERE id = 'tenant1';
+UPDATE tenant
+SET issuer = 'http://localhost:9090/realms/tenant-2' WHERE id = 'tenant2';
